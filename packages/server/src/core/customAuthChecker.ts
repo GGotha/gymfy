d3fc58bf94ff425ee3028612440a88d1d4841d6a
GGotha <clashgustavo1@gmail.com>
2022-09-05 18:57:13 -0300

    fix(server): Changing interface to type

--- packages/server/src/core/customAuthChecker.ts
+++ packages/server/src/core/customAuthChecker.ts
@@ -3,13 +3,13 @@ import { AuthChecker } from "type-graphql";
 import { prisma } from "~/externals/orm";
 import { TOKEN_SECRET } from "./constants";
 
-interface TokenPayload {
+type TokenPayloadType = {
   iat: number;
   exp: number;
   sub: {
     uuid: string;
   };
-}
+};
 
 const customAuthChecker: AuthChecker = async ({ context }: any, roles) => {
   const authHeader = context.req.headers.authorization;
@@ -22,7 +22,7 @@ const customAuthChecker: AuthChecker = async ({ context }: any, roles) => {
 
   try {
     const decoded = verify(token, TOKEN_SECRET as string);
-    const { sub } = decoded as unknown as TokenPayload;
+    const { sub } = decoded as unknown as TokenPayloadType;
 
     const user = await prisma.user.findFirst({ where: { id: sub.uuid }, include: { role: true } });
 
--- packages/server/src/core/token.ts
+++ packages/server/src/core/token.ts
@@ -2,20 +2,20 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { TOKEN_EXPIRES_IN, TOKEN_SECRET } from "./constants";
 
-interface generateTokenType {
+type GenerateTokenType = {
   userId: string;
   role: string;
-}
+};
 
-interface checkPasswordType {
+type CheckPasswordType = {
   bodyPassword: string;
   userPassword: string;
-}
+};
 
-export const generateToken = ({ userId, role }: generateTokenType): string =>
+export const generateToken = ({ userId, role }: GenerateTokenType): string =>
   jwt.sign({ sub: { uuid: userId, role } }, TOKEN_SECRET as string, {
     expiresIn: TOKEN_EXPIRES_IN,
   });
 
-export const isCorrectPassword = async ({ bodyPassword, userPassword }: checkPasswordType) =>
+export const isCorrectPassword = async ({ bodyPassword, userPassword }: CheckPasswordType) =>
   bcrypt.compare(bodyPassword, userPassword);
