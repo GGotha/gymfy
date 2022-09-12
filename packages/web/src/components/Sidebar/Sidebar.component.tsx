0de182ef6659dd0ad9375b81bcc12e49acf0d1b3
GGotha <clashgustavo1@gmail.com>
2022-09-12 18:48:06 -0300

    fix(web): Fixing some issues in Sidebar

--- packages/web/src/components/Sidebar/Sidebar.component.tsx
+++ packages/web/src/components/Sidebar/Sidebar.component.tsx
@@ -15,17 +15,35 @@ import {
 import { useState } from "react";
 import { OptionSidebarComponent } from "~/components/OptionSidebar";
 
-const SIDEBAR_LIST = [
-  { name: "Dashboard", icon: houseIcon },
-  { name: "Auctions", icon: hammerIcon },
-  { name: "Inbox", icon: mailIcon },
-  { name: "Favorites", icon: heartIcon },
-  { name: "Wallets", icon: plusIcon },
-  { name: "Bitcoin", icon: bitcoinIcon },
-  { name: "Ethereum", icon: ethereumIcon },
-  { name: "USD Coin", icon: usdcoinIcon },
-  { name: "Settings", icon: settingsIcon },
-  { name: "Help & FAQ", icon: helpIcon },
+type SidebarListProps = {
+  name: string;
+  icon: string;
+};
+
+enum SidebarEnum {
+  dashboard = "Dashboard",
+  marketplace = "Marketplace",
+  inbox = "Inbox",
+  favorites = "Favorites",
+  wallets = "Wallets",
+  bitcoin = "Bitcoin",
+  ethereum = "Ethereum",
+  usdCoin = "USD Coin",
+  settings = "Settings",
+  helpFaq = "Help & FAQ",
+}
+
+const SIDEBAR_LIST: Array<SidebarListProps> = [
+  { name: SidebarEnum.dashboard, icon: houseIcon },
+  { name: SidebarEnum.marketplace, icon: hammerIcon },
+  { name: SidebarEnum.inbox, icon: mailIcon },
+  { name: SidebarEnum.favorites, icon: heartIcon },
+  { name: SidebarEnum.wallets, icon: plusIcon },
+  { name: SidebarEnum.bitcoin, icon: bitcoinIcon },
+  { name: SidebarEnum.ethereum, icon: ethereumIcon },
+  { name: SidebarEnum.usdCoin, icon: usdcoinIcon },
+  { name: SidebarEnum.settings, icon: settingsIcon },
+  { name: SidebarEnum.helpFaq, icon: helpIcon },
 ];
 
 const SidebarComponent: React.FC = () => {
@@ -37,16 +55,16 @@ const SidebarComponent: React.FC = () => {
         <div className="mt-6 mb-12 w-full">
           <img src={gymfyIcon} alt="" />
         </div>
-        {SIDEBAR_LIST.map((option, index) => (
+        {SIDEBAR_LIST.map((option, index: number) => (
           <>
             <OptionSidebarComponent
               name={option.name}
               icon={option.icon}
               onClick={() => setCurrentIndex(index)}
-              isDashboard={index === currentIndex}
+              isDashboard={index === currentIndex && option.name !== SidebarEnum.wallets}
               hasNotifications={index === 2}
             />
-            {index === 3 || index === 7 ? (
+            {option.name === SidebarEnum.favorites || option.name === SidebarEnum.usdCoin ? (
               <div className="border border-sidebarDivider w-full my-8" />
             ) : (
               ""
