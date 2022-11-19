8cf7dcf9c5daa3823211f5225c5cf70e8f4762bb
GGotha <clashgustavo1@gmail.com>
2022-11-19 17:47:33 -0300

    fix(web): Fixing several decimals in dropdown

--- packages/web/src/components/MenuDropdownNavbar/MenuDropdownNavbar.component.tsx
+++ packages/web/src/components/MenuDropdownNavbar/MenuDropdownNavbar.component.tsx
@@ -51,7 +51,8 @@ const MenuDropdownNavbarComponent: React.FC<MenuDropdownNavbarComponentProps> = 
                   <div className="flex justify-center items-center mb-1">
                     <CircleStackIcon className="h-5 fill-yellow-400 mr-2" aria-hidden="true" />
                     <h1 className="text-white">
-                      {balance.gyc_amount} <span className="text-xs">gyc</span>
+                      {balance.gyc_amount ? balance.gyc_amount.toFixed(2) : 0}
+                      <span className="text-xs">gyc</span>
                     </h1>
                   </div>
                 )}
